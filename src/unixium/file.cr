f4f7fbd989ea6fab196c07e3c4bfee7b79aa7841
Fotis Gimian <fgimiansoftware@gmail.com>
2018-07-01 18:13:53 +1000

    Corrected references to the File module in the Unixium::File

--- src/unixium/file.cr
+++ src/unixium/file.cr
@@ -31,7 +31,7 @@ module Unixium::File
     # A symlink can never be a mount point
     return false if stat_path.st_mode & LibC::S_IFMT == LibC::S_IFLNK
 
-    parent = File.real_path(File.join(path, ".."))
+    parent = ::File.real_path(::File.join(path, ".."))
     begin
       stat_parent = stat(parent, follow_symlinks: false)
     rescue Errno
@@ -62,18 +62,18 @@ module Unixium::File
   end
 
   # Return a relative version of a path
-  def self.relative_path(path : String, start : Nil | String = nil)
+  def self.relative_path(path : String, start : String? = nil)
     current_dir = "."
     parent_dir = ".."
 
     start = current_dir unless start
 
     # Ensure both paths are absolute
-    path = File.join(Dir.current, path) unless path.starts_with?(File::SEPARATOR)
-    path = File.expand_path(path)
+    path = ::File.join(Dir.current, path) unless path.starts_with?(File::SEPARATOR)
+    path = ::File.expand_path(path)
 
-    start = File.join(Dir.current, start) unless start.starts_with?(File::SEPARATOR)
-    start = File.expand_path(start)
+    start = ::File.join(Dir.current, start) unless start.starts_with?(File::SEPARATOR)
+    start = ::File.expand_path(start)
 
     # Break up paths into their parts (ensuring no empty parts)
     path_parts = path.split(File::SEPARATOR).reject { |p| p.empty? }
@@ -83,8 +83,6 @@ module Unixium::File
     index = common_prefix([start_parts, path_parts]).size
     relative_parts = [parent_dir] * (start_parts.size - index) + path_parts[index..-1]
 
-    return current_dir if relative_parts.empty?
-
-    File.join(relative_parts)
+    relative_parts.empty? ? current_dir : ::File.join(relative_parts)
   end
 end
